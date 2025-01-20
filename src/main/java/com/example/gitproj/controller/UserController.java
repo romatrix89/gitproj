72c4838a5dd3d1db6a9ecdb996563ae5d2f73ad7
RomanTsagolov <rtsagolov@gmail.com>
2025-01-20 19:33:00 +0300

    DZ-3 Доработка логики работы с пользователем

--- src/main/java/com/example/gitproj/controller/UserController.java
+++ src/main/java/com/example/gitproj/controller/UserController.java
@@ -1,4 +1,19 @@
 package com.example.gitproj.controller;
 
+import com.example.gitproj.model.Users;
+import com.example.gitproj.service.UserService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    void createUser(Users user) {
+        userService.createUser(user);
+    }
 }
--- src/main/java/com/example/gitproj/service/UserService.java
+++ src/main/java/com/example/gitproj/service/UserService.java
@@ -1,4 +1,12 @@
 package com.example.gitproj.service;
 
+import com.example.gitproj.model.Users;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    public void createUser(Users user){
+        System.out.println(user);
+    }
 }
