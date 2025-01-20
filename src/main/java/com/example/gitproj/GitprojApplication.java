20383e3becc9bfb01b9dc523ce504db0ab1a31f4
RomanTsagolov <rtsagolov@gmail.com>
2025-01-20 19:12:30 +0300

    initial commit

---
+++ src/main/java/com/example/gitproj/GitprojApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitproj/GitprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
